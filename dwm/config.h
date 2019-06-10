46fec38784a2dd3c943a83bb8ad867abe4b43b05
mjturt <maks.turtiainen@gmail.com>
2019-06-10 19:09:03 +0300

    Centered master and keybindings

--- dwm/config.h
+++ dwm/config.h
@@ -47,6 +47,8 @@ static const Layout layouts[] = {
     { "[]=",      tile },    /* first entry is default */
     { "><>",      NULL },    /* no layout function means floating behavior */
     { "[M]",      monocle },
+    { "|M|",      centeredmaster },
+    { ">M>",      centeredfloatingmaster },
 };
 
 /* key definitions */
@@ -73,13 +75,15 @@ static Key keys[] = {
     { MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-    { MODKEY|ShiftMask,                       XK_u,      incnmaster,     {.i = +1 } },
-    { MODKEY|ShiftMask,                       XK_i,      incnmaster,     {.i = -1 } },
-    { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-    { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-    { MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
+    { MODKEY,                       XK_h,      focusstack,     {.i = +1 } },
+    { MODKEY,                       XK_l,      focusstack,     {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_u,      incnmaster,     {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_i,      incnmaster,     {.i = -1 } },
+    { MODKEY|ControlMask,           XK_h,      setmfact,       {.f = -0.02} },
+    { MODKEY|ControlMask,           XK_l,      setmfact,       {.f = +0.02} },
+    { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
     { MODKEY,                       XK_Tab,    view,           {0} },
-    { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+    { MODKEY,                       XK_w,      killclient,     {0} },
     { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
     { MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
     { MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -92,19 +96,21 @@ static Key keys[] = {
     { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
     { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
     TAGKEYS(                        XK_1,                      0)
-        TAGKEYS(                        XK_2,                      1)
-        TAGKEYS(                        XK_3,                      2)
-        TAGKEYS(                        XK_4,                      3)
-        TAGKEYS(                        XK_5,                      4)
-        TAGKEYS(                        XK_6,                      5)
-        TAGKEYS(                        XK_7,                      6)
-        TAGKEYS(                        XK_8,                      7)
-        TAGKEYS(                        XK_9,                      8)
-        { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-        { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
-        { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
-        { MODKEY,                       XK_u,           shiftview,      { .i = -1 } },
-        { MODKEY,                       XK_i,   shiftview,      { .i = 1 } },
+    TAGKEYS(                        XK_2,                      1)
+    TAGKEYS(                        XK_3,                      2)
+    TAGKEYS(                        XK_4,                      3)
+    TAGKEYS(                        XK_5,                      4)
+    TAGKEYS(                        XK_6,                      5)
+    TAGKEYS(                        XK_7,                      6)
+    TAGKEYS(                        XK_8,                      7)
+    TAGKEYS(                        XK_9,                      8)
+    { MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+    { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
+    { MODKEY|ShiftMask,             XK_h,      movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_l,      movestack,      {.i = -1 } },
+    { MODKEY,                       XK_u,      shiftview,      { .i = -1 } },
+    { MODKEY,                       XK_i,      shiftview,      { .i = 1 } },
 };
 
 /* button definitions */
