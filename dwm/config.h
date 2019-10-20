24a6e58ad5947dd55613793f2f5ff3ced1956ce1
mjturt <maks.turtiainen@gmail.com>
2019-10-20 16:34:21 +0300

    DWM: borders, gaps

--- dwm/config.h
+++ dwm/config.h
@@ -1,19 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int panel[] = {0, 25, 0, 0};//positions: 0-top panel, 1-bottom panel, 2-left panel, 3-right panel
+static const unsigned int panel[]   = {0, 25, 0, 0}; //positions: 0-top panel, 1-bottom panel, 2-left panel, 3-right panel
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;       /* gap pixel between windows */
+static const unsigned int gappx     = 0;       /* gap pixel between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Misc Ohsnap:size=10", "GohuFont Nerd Font:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
+static const unsigned int baralpha  = 0xd0;
+static const unsigned int borderalpha = OPAQUE;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_red[]         = "#aa4450";
 static const char col_gold[]        = "#87875f";
 static const char col_green[]       = "#719611";
 static const char col_orange[]      = "#ff9800";
@@ -21,7 +24,13 @@ static const char col_orange[]      = "#ff9800";
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gold, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_orange, col_gray1,  col_cyan  },
+    [SchemeSel]  = { col_orange, col_gray1,  col_red  },
+};
+
+static const unsigned int alphas[][3]      = {
+    /*               fg      bg        border     */
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+    [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
